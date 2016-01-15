19cc6a5e1dedca28c3b34cfee93dfe0237de780e
Augusto Goncalves <augusto.goncalves@autodesk.com>
2016-01-15 17:08:30 -0200

    Update Credentials.cs

--- FirstViewerWebApp/FirstViewerWebApp/Credentials.cs
+++ FirstViewerWebApp/FirstViewerWebApp/Credentials.cs
@@ -3,8 +3,8 @@
   public class Credentials
   {
     //replace your consumer key and secret key
-    public static string CONSUMER_KEY = "Cxv0RgtyfH2J1sJrnrKcwSBRXRmhagY6";//"please request your consumer key at http://developer.autodesk.com";
-    public static string CONSUMER_SECRET = "F5c49994f13e843e";//"not set";
+    public static string CONSUMER_KEY = "please request your consumer key at http://developer.autodesk.com";
+    public static string CONSUMER_SECRET = "not set";
     public static string BASE_URL = "https://developer.api.autodesk.com";
   }
 }
